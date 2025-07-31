9dfe57c7035459245ace12c69279001dc6d91013
Sujan R <NTIN10@Naksha.Tech>
2025-07-31 13:21:39 +0530

    Remove large video files from Git tracking

--- backend/check.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-import cv2
-
-model = YOLO("models/best.pt")
-img = cv2.imread("C:/Users/NTIN10/Pictures/Screenshots/image.jpg")  # replace with a test image
-
-results = model.predict(img)
-results[0].show()  # shows image with boxes
-
